31449ebac8e06005d33d2d31ed33ba00fb15076d
ferdi <ferdid047@gmail.com>
2023-05-11 13:54:04 +0700

    UTS_22090142_MOHAMMAD FERDI DARMAWAN_2E

---
+++ assets/requires/config.php
@@ -0,0 +1,6 @@
+<?php
+define('APPNAME','Harber Web');
+define('PATH','http://localhost/');
+define('SITE ROBOT',__DIR__);
+define('MYROOT',$_SERVER['DOCUMENT_ROOT']);
+?>
---
+++ assets/requires/header1.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<meta http_equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php echo APPNAME; ?></title>
+<!-- Bootstrap -->
+
+<link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./bootstrap/css/sweetalert.css">
+
+<!-- Jquery and Bootstrap Scripts Loading -->
+<script src="./JQuery/JQuery-3.5.1.js"></script>
+<script src="./bootstrap/js/bootstrap.js"></script>
+<script src="./JQuery/js/bootstrap.min.js"></script>
+<script src="./JQuery/sweetalert.min.js"></script>
